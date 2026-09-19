04d3fd25d4406a86775af2e4253028ee9a2d089b
MisaqArtist7 <misaqbabakhani777@gmail.com>
2026-09-19 19:40:17 +0330

    feat: design acardeon for faq page

--- app/(marketing)/faq/page.tsx
+++ app/(marketing)/faq/page.tsx
@@ -1,7 +1,77 @@
-import React from 'react'
+'use client'
 
-export default function page() {
+import { useState } from "react"
+
+export default function FaqPage() {
+
+  const [faqs, setFaqs] = useState([
+    {
+      id: 1,
+      title: "کد ورود (پیامک) برایم ارسال نمی‌شود چرا؟",
+      answer:
+        "گاهی کد در مسیر مخابرات با تأخیر مواجه می‌شود؛ لطفاً چند بار و با فاصله‌ی زمانی امتحان کنید و پوشه‌ی پیامک‌های اسپم (هرزنامه) را نیز بررسی کنید. در صورت ادامه‌ی مشکل، احتمالاً شماره‌ی ما را پیش‌تر مسدود کرده‌اید (به دلیل پیام‌های تبلیغاتی سایر شرکت‌ها)؛ در این حالت از شماره‌ی دیگری استفاده کنید.",
+      isOpen: false,
+    }
+  ])
+
+  const handleToggle = (id : number) => {
+    setFaqs(
+      faqs.map(item => item.id === id ? {...item, isOpen : !item.isOpen} : item)
+    )
+  }
+
   return (
-    <div>page</div>
+    <div className="container mx-auto flex flex-col items-center gap-5 py-10">
+      {faqs.map((item) => (
+        <div
+          key={item.id}
+          className="w-full max-w-3xl overflow-hidden rounded-2xl border shadow-md my-11"
+          style={{
+            backgroundColor: "var(--bg-card)",
+            borderColor: "var(--border-color)",
+          }}
+        >
+          {/* Question */}
+          <div onClick={() => handleToggle(item.id)}
+            className="flex cursor-pointer items-center justify-between gap-4 p-5 transition hover:bg-(--bg-subtle)"
+          >
+            <h2
+              className="text-right text-lg font-bold leading-8"
+              style={{ color: "var(--text-main)" }}
+            >
+              {item.title}
+            </h2>
+
+            <span
+              className="flex h-10 w-10 shrink-0 items-center justify-center rounded-full transition"
+              style={{
+                backgroundColor: "var(--bg-subtle)",
+                color: "var(--text-muted)",
+              }}
+            >
+              <svg className={`w-5 h-5 ${item.isOpen && 'rotate-180'} transition-all ease-in`}>
+                <use href="#chevron-down"></use>
+              </svg>
+            </span>
+          </div>
+
+          {/* Answer */}
+          {item.isOpen && (
+            <div
+              className="border-t px-5 py-5"
+              style={{ borderColor: "var(--border-color)" }}
+            >
+              <p
+                className="text-justify leading-8"
+                style={{ color: "var(--text-muted)" }}
+              >
+                {item.answer}
+              </p>
+            </div>
+          )}
+
+        </div>
+      ))}
+    </div>
   )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,9 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
         <symbol id="doorIcon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
           <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15M12 9l3 3m0 0-3 3m3-3H2.25" />
         </symbol>
-
+        <symbol id="chevron-down" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="size-6">
+          <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+        </symbol>
       </svg>
       <body>{children}</body>
     </html>
